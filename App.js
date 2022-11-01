e8ffa0889e6cff0a2df796a4e68ccb9c01f3d51f
drewhb <74934734+drewhb@users.noreply.github.com>
2022-11-01 13:56:43 -0400

    creating checkbox logic

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, View } from "react-native";
+import { useState } from "react";
+import CheckBox from "./CheckBox";
+  
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
+    const [music, setMusic] = useState(false);
+      const [dancing, setDancing] = useState(false);
+      const [reading, setReading] = useState(false);
+  
+    return (
+        <View style={styles.container}>
+            <CheckBox
+                onPress={() => setMusic(!music)}
+                title="Music"
+                isChecked={music}
+              />
+              <CheckBox
+                onPress={() => setDancing(!dancing)}
+                title="Dancing"
+                isChecked={dancing}
+              />
+              <CheckBox
+                onPress={() => setReading(!reading)}
+                title="Reading"
+                isChecked={reading}
+              />
+        </View>
+      );
+    }
+  
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        justifyContent: "center",
+        alignItems: "center",
+      },
 });
