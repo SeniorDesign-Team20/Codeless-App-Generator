9aaa8ab98fe666ebb53ec0ceebf20e688bd4e916
drewhb <74934734+drewhb@users.noreply.github.com>
2022-11-01 15:42:35 -0400

    adding features

--- App.js
+++ App.js
@@ -1,28 +1,41 @@
 import { StyleSheet, View } from "react-native";
 import { useState } from "react";
 import CheckBox from "./CheckBox";
-  
+//import Headers from "./TextAndHeaders";
+
 export default function App() {
-    const [music, setMusic] = useState(false);
-      const [dancing, setDancing] = useState(false);
-      const [reading, setReading] = useState(false);
-  
+    const [feature1, set1] = useState(false);
+    const [feature2, set2] = useState(false);
+    const [feature3, set3] = useState(false);
+    const [feature4, set4] = useState(false);
+    const [feature5, set5] = useState(false);
+    
     return (
         <View style={styles.container}>
             <CheckBox
-                onPress={() => setMusic(!music)}
-                title="Music"
-                isChecked={music}
+                onPress={() => set1(!feature1)}
+                title="Example Feature 1"
+                isChecked={feature1}
               />
               <CheckBox
-                onPress={() => setDancing(!dancing)}
-                title="Dancing"
-                isChecked={dancing}
+                onPress={() => set2(!feature2)}
+                title="Example Feature 2"
+                isChecked={feature2}
               />
               <CheckBox
-                onPress={() => setReading(!reading)}
-                title="Reading"
-                isChecked={reading}
+                onPress={() => set3(!feature3)}
+                title="Example Feature 3"
+                isChecked={feature3}
+              />
+              <CheckBox
+                onPress={() => set4(!feature4)}
+                title="Example Feature 4"
+                isChecked={feature4}
+              />
+              <CheckBox
+                onPress={() => set5(!feature5)}
+                title="Example Feature 5"
+                isChecked={feature5}
               />
         </View>
       );
@@ -33,6 +46,6 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "#fff",
         justifyContent: "center",
-        alignItems: "center",
+        alignItems: "left",
       },
 });
