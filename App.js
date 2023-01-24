edd673d65188e1214dd67ba9bfb29683c1f1a17c
drewhb <74934734+drewhb@users.noreply.github.com>
2023-01-24 15:48:30 -0500

    Implementation of remove feature logic

--- App.js
+++ App.js
@@ -36,6 +36,10 @@ export default function App() {
       {
         setFeatures(arr => [...arr, "Feature1"])
       }
+      else{
+        const index = selectedFeatures.indexOf("Feature1")
+        selectedFeatures.splice(index, 1)
+      }
       console.log(selectedFeatures.toString())
     }
     const setFeat2 = (feat) => {
@@ -43,6 +47,10 @@ export default function App() {
       if (!feat)
       {
         setFeatures(arr => [...arr, "Feature2"])
+      }
+      else{
+        const index = selectedFeatures.indexOf("Feature2")
+        selectedFeatures.splice(index, 1)
       }
       console.log(selectedFeatures.toString())
     }
@@ -52,6 +60,10 @@ export default function App() {
       {
         setFeatures(arr => [...arr, "Feature3"])
       }
+      else{
+        const index = selectedFeatures.indexOf("Feature3")
+        selectedFeatures.splice(index, 1)
+      }
       console.log(selectedFeatures.toString())
     }
     const setFeat4 = (feat) => {
@@ -59,6 +71,10 @@ export default function App() {
       if (!feat)
       {
         setFeatures(arr => [...arr, "Feature4"])
+      }
+      else{
+        const index = selectedFeatures.indexOf("Feature4")
+        selectedFeatures.splice(index, 1)
       }
       console.log(selectedFeatures.toString())
     }
@@ -68,6 +84,10 @@ export default function App() {
       {
         setFeatures(arr => [...arr, "Feature5"])
       }
+      else{
+        const index = selectedFeatures.indexOf("Feature5")
+        selectedFeatures.splice(index, 1)
+      }
       console.log(selectedFeatures.toString())
     }
 
@@ -75,57 +95,74 @@ export default function App() {
     const confirmFeatures = (inputText) => {
         setFeatures(arr => [...arr, inputText])
     }
+    const removeFeature = () => {
+        setFeatures(arr => arr.slice(0, -1))
+    }
     const redirect = (val) =>(
       EnterText(val)
     )
 
     return (
         <View style={styles.container}>
-            <Top style = {styles.titleStyle}/>
-            <View syle={styles.chooseFeaturesContainer}>
-                <Checkbox
-                  onPress = {() =>{setFeat1(feature1)}}
-                  title="Example Feature 1"
-                  isChecked={feature1}
-                />
-                <Checkbox
-                  onPress = {() => setFeat2(feature2)}
-                  title="Example Feature 2"
-                  isChecked={feature2}
-                />
-                <Checkbox
-                  onPress={() => setFeat3(feature3)}
-                  title="Example Feature 3"
-                  isChecked={feature3}
-                />
-                <Checkbox
-                  onPress={() => setFeat4(feature4)}
-                  title="Example Feature 4"
-                  isChecked={feature4}
-                />
-                <Checkbox
-                  onPress={() => setFeat5(feature5)}
-                  title="Example Feature 5"
-                  isChecked={feature5}
-                />    
-                <TextInput
-                  style = {styles.enter}
-                  placeholder ='Enter Features Here ... '
-                  onChangeText={redirect}>
-                </TextInput>
-                <Button 
-                  style = {styles.ConfirmButton}
-                  title="Confirm" 
-                  color ='red'
-                  onPress={() => confirmFeatures(defaultText)}
-                />
+            <View style = {styles.titleContainer}>
+              <Top style = {styles.titleStyle}/>
             </View>
-            <View style = {styles.displaySelectionContainer}>
-                <Text> You selected: {selectedFeatures.toString()}</Text>
-            </View>
-            
+            <View style = {styles.contentContainer}>
+              <View syle={styles.chooseFeaturesContainer}>
+                  <Checkbox
+                    onPress = {() =>{setFeat1(feature1)}}
+                    title="Example Feature 1"
+                    isChecked={feature1}
+                  />
+                  <Checkbox
+                    onPress = {() => setFeat2(feature2)}
+                    title="Example Feature 2"
+                    isChecked={feature2}
+                  />
+                  <Checkbox
+                    onPress={() => setFeat3(feature3)}
+                    title="Example Feature 3"
+                    isChecked={feature3}
+                  />
+                  <Checkbox
+                    onPress={() => setFeat4(feature4)}
+                    title="Example Feature 4"
+                    isChecked={feature4}
+                  />
+                  <Checkbox
+                    onPress={() => setFeat5(feature5)}
+                    title="Example Feature 5"
+                    isChecked={feature5}
+                  />    
+                  <TextInput
+                    style = {styles.enter}
+                    placeholder ='Enter A Feature Here ... '
+                    onChangeText={redirect}>
+                  </TextInput>
+                  <Button 
+                    style = {styles.ConfirmButton}
+                    title="Add Feature" 
+                    color ='green'
+                    onPress={() => confirmFeatures(defaultText)}
+                  />
+                  <Text style = {{flex: 1, flexWrap: "wrap", flexShrink:1}}> You selected: {selectedFeatures.toString()}</Text>
+                  <Button
+                    title="Remove Feature"
+                    color='red'
+                    onPress={() => removeFeature()}
+                  />
+                  <Button
+                    title="Confirm Selections"
+                    color='green'
+                  />
 
+              </View>
+              <View style = {styles.displaySelectionContainer}>
+              </View>
+            </View>
         </View>
+        
+        
       );
     }
   
@@ -133,16 +170,32 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: "column",
+        flexDirection: "col",
         backgroundColor: "#fff",
         justifyContent: "flex-start",
         alignItems: "flex-start"
         //top: 0
       },
+      titleContainer: {
+        flexDirection: "row",
+        alignSelf: "baseline",
+        width: "100%"
+      },
+      contentContainer: {
+        flex: 5,
+        flexDirection: "row",
+        // justifyContent: "space-between",
+        // alignContent: "center"
+      },
     chooseFeaturesContainer: {
+      flex: 1,
       backgroundColor: "#fff",
-      justifyContent: "center",
-      alignItems: "flex-start",
+      justifyContent: "flex-start",
+      alignItems: "space-between",
+      flexDirection: "row",
+      width: "50%",
+      flexWrap: "wrap",
+      
     },
     titleStyle: {
       //flex: 1,
@@ -150,12 +203,14 @@ const styles = StyleSheet.create({
       top: 0
     },
     ConfirmButton:{
-      //padding: 20,
+      marginTop: 100,
+      marginBottom: 100
       //fontSize: 15,
       //fontFamily: "arial",
       
       //justifyContent: "center"
-      alignSelf: "stretch"
+      //alignSelf: "stretch",
+      //width: "50%"
     },
     item: {
       padding: 10,
@@ -166,14 +221,15 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: "#fff",
       justifyContent: "flex-start",
-      alignItems:"flex-start",
+      alignSelf:"flex-start",
       flexDirection: "row"
     },
+
     enter:{
       textAlign: 'flex-end',
       fontSize:'16',
       marginBottom: 10,
-      marginTop:100,
+      marginTop:50,
       borderBottomWidth: 1,
       borderBottomColor: 'grey',
       width: 400},
