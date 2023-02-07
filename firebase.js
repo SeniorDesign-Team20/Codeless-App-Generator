4f23a3b3f8fb3a7a49bb0073bc6e7ca01fddb1a7
mvinci12 <75142989+mvinci12@users.noreply.github.com>
2023-02-07 15:54:23 -0500

    fetch error debugging - working otherwise

--- firebase.js
+++ firebase.js
@@ -5,19 +5,19 @@ import { getStorage, ref, getDownloadURL } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const firebaseConfig = {
+      apiKey: "${{ secrets.FIREBASEAPIKEY }}",
+      authDomain: "codeless-app-generator.firebaseapp.com",
+      projectId: "codeless-app-generator",
+      storageBucket: "codeless-app-generator.appspot.com",
+      messagingSenderId: "${{ secrets.MESSAGINGSENDERID }}",
+      appId: "${{ secrets.APPID }}"
+};
 
 
-const Firebase = () => {
-      // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "${{ secrets.FIREBASEAPIKEY }}",
-    authDomain: "codeless-app-generator.firebaseapp.com",
-    projectId: "codeless-app-generator",
-    storageBucket: "codeless-app-generator.appspot.com",
-    messagingSenderId: "${{ secrets.MESSAGINGSENDERID }}",
-    appId: "${{ secrets.APPID }}"
-  };
+
+
+const Firebase = async () => {
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
@@ -25,58 +25,149 @@ const Firebase = () => {
   const storage = getStorage(app);
   // const db = getDatabase(app);
 
+  const fileNames = ['/Weather.js', '/firebase_test.txt'];
+  const promises = fileNames.map(async fileName => {
+    const fileRef = ref(storage,fileName)
+    console.log("got reference");
+    const url = await getDownloadURL(fileRef);
+    console.log("got download URL");
+    console.log(url);
+    // Trying Fetch
+    try {
+      const response = await fetch(url, {mode: 'cors'});
+      console.log(response);
+      console.log(response.status);
+    } catch (error) {
+      console.error('An error occurred while making the fetch request: ', error);
+      console.log(response.status);
+    }
+
+    if (!response.ok) {
+      const message = `Response: An error has occured: ${response.status}`;
+      throw new Error(message);
+    }  
+    console.log("got response");
+    const blob = await response.blob();
+    return { fileName, blob };
+  });
+
+  const files = await Promise.all(promises);
+  const zip = new JSZip();
+  files.forEach(({ fileName, blob }) => {
+    zip.file(fileName, blob);
+    console.log("zipped");
+  });
+
+  const zipBlob = await zip.generateAsync({ type: 'blob' });
+  const zipRef = ref(storage, 'Features.zip/');
+  const snapshot = await uploadBytes(zipRef, zipBlob);
+  console.log('Uploaded a blob or file!');
+  const downloadURL = await getDownloadURL(zipRef);
+  return downloadURL;
+};
+
+export default Firebase;
 
 
-  const storageRef = ref(storage);
-  const textFileRef = ref(storage, '/Features.zip');
 
-  const url = getDownloadURL(textFileRef);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Firebase = () => {
+//       // Your web app's Firebase configuration
+//   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//   const firebaseConfig = {
+//     apiKey: "${{ secrets.FIREBASEAPIKEY }}",
+//     authDomain: "codeless-app-generator.firebaseapp.com",
+//     projectId: "codeless-app-generator",
+//     storageBucket: "codeless-app-generator.appspot.com",
+//     messagingSenderId: "${{ secrets.MESSAGINGSENDERID }}",
+//     appId: "${{ secrets.APPID }}"
+//   };
+
+//   // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
+//   // Initialize Cloud Storage and get a reference to the service
+//   const storage = getStorage(app);
+//   // const db = getDatabase(app);
+
+
+
+//   const storageRef = ref(storage);
+//   const textFileRef = ref(storage, '/Weather.js');
+
+//   const url = getDownloadURL(textFileRef);
   
 
-  getDownloadURL(textFileRef)
-    .then((url) => {
-      // `url` is the download URL for 'images/firebase_test.txt'
-      console.log(url);
-      const url_return = url;
-      // This can be downloaded directly:
-      const xhr = new XMLHttpRequest();
-      xhr.responseType = 'blob';
-      xhr.onload = (event) => {
-        const blob = xhr.response;
-      };
-      xhr.open('GET', url);
-      xhr.send();
-      const URL = url;
+//   getDownloadURL(textFileRef)
+//     .then((url) => {
+//       // `url` is the download URL for 'images/firebase_test.txt'
+//       console.log(url);
+//       const url_return = url;
+//       // This can be downloaded directly:
+//       const xhr = new XMLHttpRequest();
+//       xhr.responseType = 'blob';
+//       xhr.onload = (event) => {
+//         const blob = xhr.response;
+//       };
+//       xhr.open('GET', url);
+//       xhr.send();
+//       const URL = url;
 
-    })
-    .catch((error) => {
-      // Handle any errors
-      // A full list of error codes is available at
-      // https://firebase.google.com/docs/storage/web/handle-errors
-      switch (error.code) {
-        case 'storage/object-not-found':
-          // File doesn't exist
-          console.log('File doesnt exist');
-          break;
-        case 'storage/unauthorized':
-          // User doesn't have permission to access the object
-          console.log('user doesnt have permission to access object');
-          break;
-        case 'storage/canceled':
-          // User canceled the upload
-          console.log('User cancelled the upload');
-          break;
-        case 'storage/unknown':
-          // Unknown error occurred, inspect the server response
-          console.log('Unkownerror occurred, inspect the server response');
-          break;
-        default:
-          console.log(url);
-      }
-    });
-    return url;
-    // return document;
-  }
+//     })
+//     .catch((error) => {
+//       // Handle any errors
+//       // A full list of error codes is available at
+//       // https://firebase.google.com/docs/storage/web/handle-errors
+//       switch (error.code) {
+//         case 'storage/object-not-found':
+//           // File doesn't exist
+//           console.log('File doesnt exist');
+//           break;
+//         case 'storage/unauthorized':
+//           // User doesn't have permission to access the object
+//           console.log('user doesnt have permission to access object');
+//           break;
+//         case 'storage/canceled':
+//           // User canceled the upload
+//           console.log('User cancelled the upload');
+//           break;
+//         case 'storage/unknown':
+//           // Unknown error occurred, inspect the server response
+//           console.log('Unkownerror occurred, inspect the server response');
+//           break;
+//         default:
+//           console.log(url);
+//       }
+//     });
+//     return url;
+//     // return document;
+//   }
 
-  export default Firebase;
+//   export default Firebase;
   
